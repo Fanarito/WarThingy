d0f42051798bba925cf9b23eee552381e8baba6a
unknown <ingoar97@gmail.com>
2015-11-25 22:39:43 +0000

    movement boundary code and changed when mouse position is checked

--- WarThingy.py
+++ WarThingy.py
@@ -11,11 +11,12 @@ pygame.init()
 class Player(pygame.sprite.Sprite):
     def __init__(self, location):
         pygame.sprite.Sprite.__init__(self)
-
+        # player picture is 48 * 56 pixels
         self.image = pygame.image.load('images/player.png')
         self.original = self.image
         self.rect = self.image.get_rect(center=location)
         self.angle = 0
+
 
     def get_angle(self, mouse):
         """
@@ -45,17 +46,29 @@ class Main:
             if event.type == pygame.QUIT:
                 self.playing = False
 
-            if event.type == pygame.MOUSEMOTION:
-                self.player.get_angle(event.pos)
+        self.player.get_angle(pygame.mouse.get_pos())
 
+        key = pygame.key.get_pressed()
+        if (key[pygame.K_LEFT] or key[pygame.K_a]):
+            if ( 0 < self.player.rect.x ):
+                self.player.rect.centerx -= 5
+        if (key[pygame.K_RIGHT] or key[pygame.K_d]):
+            if ( screen_width > self.player.rect.x + + self.player.image.get_rect().size[0]):
+                self.player.rect.centerx += 5
+        if (key[pygame.K_UP] or key[pygame.K_w]):
+            if (0 < self.player.rect.y ):
+                self.player.rect.centery -= 5
+        if (key[pygame.K_DOWN] or key[pygame.K_s]):
+            if ( screen_height > self.player.rect.y + self.player.image.get_rect().size[1]):
+                self.player.rect.centery += 5
+        #print self.player.image.get_rect().size
         self.all_sprites.draw(self.screen)
-
         pygame.display.flip()
 
 
 if __name__ == '__main__':
     game = Main()
-
+    all_sprites = pygame.sprite.Group()
     while game.playing:
         game.clock.tick(60)
         game.main()
